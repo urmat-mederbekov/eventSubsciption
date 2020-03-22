4be743b23685f5a46b4434951428f1e60bc8a8eb
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-22 19:13:42 +0600

    added application

---
+++ src/main/java/kg/attractor/eventsubscription/EventsubscriptionApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.eventsubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsubscriptionApplication.class, args);
+    }
+
+}
