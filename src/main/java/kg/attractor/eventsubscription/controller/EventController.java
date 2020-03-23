fed83282618e9191aceb85044fb0be0d3d8cd9e8
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-23 12:53:26 +0600

    deleted extra things in paths and moved findallbyemail method from subscription to event controller

--- src/main/java/kg/attractor/eventsubscription/controller/EventController.java
+++ src/main/java/kg/attractor/eventsubscription/controller/EventController.java
@@ -1,11 +1,15 @@
 package kg.attractor.eventsubscription.controller;
 
 import kg.attractor.eventsubscription.dto.EventDTO;
+import kg.attractor.eventsubscription.dto.SubscriptionDTO;
+import kg.attractor.eventsubscription.service.SubscriptionService;
 import kz.attractorschool.microgram.annotations.ApiPageable;
 import kg.attractor.eventsubscription.service.EventService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -15,14 +19,23 @@ import springfox.documentation.annotations.ApiIgnore;
 public class EventController {
 
     private final EventService eventService;
+    private final SubscriptionService subscriptionService;
 
-    public EventController(EventService eventService) {
+    public EventController(EventService eventService, SubscriptionService subscriptionService) {
         this.eventService = eventService;
+        this.subscriptionService = subscriptionService;
     }
 
     @ApiPageable
     @GetMapping
     public Slice<EventDTO> findEvents(@ApiIgnore Pageable pageable){
         return eventService.findEvents(pageable);
+    }
+
+    @ApiPageable
+    @GetMapping("/{email}/subscriptions")
+    public Page<SubscriptionDTO> findSubscriptionByEmail(@ApiIgnore Pageable pageable,
+                                                         @PathVariable String email){
+        return subscriptionService.findSubscriptionsByEmail(pageable, email);
     }
 }
--- src/main/java/kg/attractor/eventsubscription/controller/SubscriptionController.java
+++ src/main/java/kg/attractor/eventsubscription/controller/SubscriptionController.java
@@ -2,11 +2,8 @@ package kg.attractor.eventsubscription.controller;
 
 import kg.attractor.eventsubscription.dto.SubscriptionDTO;
 import kg.attractor.eventsubscription.service.SubscriptionService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/subscriptions")
@@ -17,18 +14,12 @@ public class SubscriptionController {
         this.subscriptionService = subscriptionService;
     }
 
-    @GetMapping("{email}")
-    public Page<SubscriptionDTO> findSubscriptionByEmail(@ApiIgnore Pageable pageable,
-                                                      @PathVariable String email){
-        return subscriptionService.findSubscriptionsByEmail(pageable, email);
-    }
-
-    @PostMapping("add/{eventId}&{email}")
+    @PostMapping("/{eventId}&{email}")
     public SubscriptionDTO subscribe(@PathVariable String eventId, @PathVariable String email){
         return subscriptionService.addSubscription(eventId, email);
     }
 
-    @DeleteMapping("/delete/{subscriptionId}&{email}")
+    @DeleteMapping("/{subscriptionId}&{email}")
     public ResponseEntity<Void> unsubscribe(@PathVariable String subscriptionId,
                                             @PathVariable String email){
         if(subscriptionService.deleteSubscription(subscriptionId, email))
